cb4c97f0f25ce801e1c6ab72bd32974cfef55739
sandipsinh400 <zalasandipsinh3185@gmail.com>
2025-01-02 15:00:50 +0530

    change

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { default: mongoose } = require('mongoose')
 const app = express()
+var cors = require('cors')
 const port = 3000
 
 
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({extended:true}))
 
 app.get('/', (req, res) => res.send('Hello55 World!'))
